b5cb730e73efcc4f4797b6fa7d8058d654346c62
Harish G <harishgovind6000@gmail.com>
2025-05-06 17:20:31 +0530

    Completed day 2 tasks

---
+++ Day-2/afternoon/SQLQuery6.sql
@@ -0,0 +1,133 @@
+CREATE TABLE DEPARTMENT (
+    DeptName VARCHAR(255) PRIMARY KEY,
+    FloorNO INT,
+    Phone VARCHAR(15),
+	ManagerId INT NOT NULL
+);
+
+
+CREATE TABLE EMP (
+    EmpNo INT PRIMARY KEY,
+    EmpName VARCHAR(255) NOT NULL,
+    Salary DECIMAL(10, 2),
+    DeptName VARCHAR(255),
+    BossNo INT,
+    FOREIGN KEY (DeptName) REFERENCES DEPARTMENT(DeptName),
+    FOREIGN KEY (BossNo) REFERENCES EMP(EmpNo)
+);
+
+
+CREATE TABLE ITEM (
+    ItemName VARCHAR(255) PRIMARY KEY,
+    ItemType VARCHAR(255),
+    ItemColor VARCHAR(50)
+);
+
+
+CREATE TABLE SALES (
+    SalesNo INT PRIMARY KEY,
+    SaleQty INT NOT NULL,
+    ItemName VARCHAR(255) NOT NULL,
+    DeptName VARCHAR(255) NOT NULL,
+    FOREIGN KEY (ItemName) REFERENCES ITEM(ItemName),
+    FOREIGN KEY (DeptName) REFERENCES DEPARTMENT(DeptName)
+);
+
+INSERT INTO ITEM (ItemName, ItemType, ItemColor) VALUES
+('Pocket Knife-Nile', 'E', 'Brown'),
+('Pocket Knife-Avon', 'E', 'Brown'),
+('Compass', 'N', NULL),
+('Geo positioning system', 'N', NULL),
+('Elephant Polo stick', 'R', 'Bamboo'),
+('Camel Saddle', 'R', 'Brown'),
+('Sextant', 'N', NULL),
+('Map Measure', 'N', NULL),
+('Boots-snake proof', 'C', 'Green'),
+('Pith Helmet', 'C', 'Khaki'),
+('Hat-polar Explorer', 'C', 'White'),
+('Exploring in 10 Easy Lessons', 'B', NULL),
+('Hammock', 'F', 'Khaki'),
+('How to win Foreign Friends', 'B', NULL),
+('Map case', 'E', 'Brown'),
+('Safari Chair', 'F', 'Khaki'),
+('Safari cooking kit', 'F', 'Khaki'),
+('Stetson', 'C', 'Black'),
+('Tent - 2 person', 'F', 'Khaki'),
+('Tent -8 person', 'F', 'Khaki');
+
+INSERT INTO DEPARTMENT (DeptName, FloorNO, Phone, ManagerId)
+VALUES 
+('Management', 5, 34, 1),
+('Books', 1, 81, 4),
+('Clothes', 2, 24, 4),
+('Equipment', 3, 57, 3),
+('Furniture', 4, 14, 3),
+('Navigation', 1, 41, 3),
+('Recreation', 2, 29, 4),
+('Accounting', 5, 35, 5),
+('Purchasing', 5, 36, 7),
+('Personnel', 5, 37, 9),
+('Marketing', 5, 38, 2);
+
+INSERT INTO EMP (EmpNo, EmpName, Salary, DeptName, BossNo)
+VALUES 
+(1, 'Alice', 75000.00, 'Management', NULL),
+(2, 'Ned', 45000.00, 'Marketing', 1),
+(3, 'Andrew', 25000.00, 'Marketing', 2),
+(4, 'Clare', 22000.00, 'Marketing', 2),
+(5, 'Todd', 38000.00, 'Accounting', 1),
+(6, 'Nancy', 22000.00, 'Accounting', 5),
+(7, 'Brier', 43000.00, 'Purchasing', 1),
+(8, 'Sarah', 56000.00, 'Purchasing', 7),
+(9, 'Sophile', 35000.00, 'Personnel', 1),
+(10, 'Sanjay', 15000.00, 'Navigation', 3),
+(11, 'Rita', 15000.00, 'Books', 4),
+(12, 'Gigi', 16000.00, 'Clothes', 4),
+(13, 'Maggie', 11000.00, 'Clothes', 4),
+(14, 'Paul', 15000.00, 'Equipment', 3),
+(15, 'James', 15000.00, 'Equipment', 3),
+(16, 'Pat', 15000.00, 'Furniture', 3),
+(17, 'Mark', 15000.00, 'Recreation', 3);
+
+INSERT INTO SALES (SalesNo, SaleQty, ItemName, DeptName)
+VALUES 
+(101, 2, 'Boots-snake proof', 'Clothes'),
+(102, 1, 'Pith Helmet', 'Clothes'),
+(103, 1, 'Sextant', 'Navigation'),
+(104, 3, 'Hat-polar Explorer', 'Clothes'),
+(105, 5, 'Pith Helmet', 'Equipment'),
+(106, 2, 'Pocket Knife-Nile', 'Clothes'),
+(107, 3, 'Pocket Knife-Nile', 'Recreation'),
+(108, 1, 'Compass', 'Navigation'),
+(109, 2, 'Geo positioning system', 'Navigation'),
+(110, 5, 'Map Measure', 'Navigation'),
+(111, 1, 'Geo positioning system', 'Books'),
+(112, 1, 'Sextant', 'Books'),
+(113, 3, 'Pocket Knife-Nile', 'Books'),
+(114, 1, 'Pocket Knife-Nile', 'Navigation'),
+(115, 1, 'Pocket Knife-Nile', 'Equipment'),
+(116, 1, 'Sextant', 'Clothes'),
+(117, 1, 'Sextant', 'Equipment'),
+(118, 1, 'Sextant', 'Recreation'),
+(119, 1, 'Sextant', 'Furniture'),
+(120, 1, 'Pocket Knife-Nile', 'Furniture'),
+(121, 1, 'Exploring in 10 easy lessons', 'Books'),
+(122, 1, 'How to win foreign friends', 'Books'),
+(123, 1, 'Compass', 'Books'),
+(124, 1, 'Pith Helmet', 'Books'),
+(125, 1, 'Elephant Polo stick', 'Recreation'),
+(126, 1, 'Camel Saddle', 'Recreation');
+
+ALTER TABLE DEPARTMENT
+ADD CONSTRAINT FK_Manager FOREIGN KEY (ManagerId) REFERENCES EMP(EmpNo);
+
+SELECT * FROM EMP;
+
+SELECT * FROM DEPARTMENT;
+
+SELECT * FROM SALES;
+
+SELECT * FROM ITEM;
+
+
+
